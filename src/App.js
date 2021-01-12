167f954691ee9f29205f630ef92550373cec6ea3
yujoh <dbwjd090@gmail.com>
2021-01-13 00:13:12 +0900

    Change function component to class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+
+//#region function component
 // how to use props
+/*
 function Food({ name, image, ratings }) {
   return (
     <div>
@@ -62,6 +65,33 @@ function App() {
       ))}
     </div>
   );
+}
+*/
+//#endregion
+
+// class component
+
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  add = () => {
+    console.log("add")
+  };
+  minus = () => {
+    console.log("minus")
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
